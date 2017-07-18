ec405f06dff8d49211c2139101b6fbb212f73d53
Sean Kelly <seanpkelly06@gmail.com>
2017-07-18 00:00:38 -0400

    adding blog index

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,11 @@ use App\Post;
 
 class BlogController extends Controller
 {
+	public function getIndex() {
+		$posts = Post::paginate(10);
+		return view('blog.index')->withPosts($posts);
+
+	}
     //fetch from the db based on slug
     //return the view and past the post object
     public function getSingle($slug) {
