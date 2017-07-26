e5e92835426dc7fd34c8bc5448cb6f0ace3756a8
Sean Kelly <seanpkelly06@gmail.com>
2017-07-26 05:20:03 -0400

    Much needed changes to front end

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,15 +4,13 @@
 @section('meta_description', "Hello! If you need a full stack developer or just want to say hello, feel free to drop me a line!")
 @section('content')
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-8 col-md-offset-2">
           <div class="jumbotron">
-            <h1>Contact</h1>
-            <p class="lead">Thank you for visiting. This project has been built with Laravel. Please read my popular post</p>
-            <p><a href="#" class="btn btn-primary btn-lg">Contact</a></p>
+            <h1>Hello!</h1>
+            <p class="lead">Thank you for visiting. We should talk...</p>
           </div> <!-- end of jumbrotron -->
         </div> <!-- end of columns -->
-      </div> <!-- end of row -->
-      <h3>My mail server is Mailtrap, so if you send me something -- I won't get it!</h3>
+        <div class="col-md-8 col-md-offset-2">
       <form action="{{ url('contact') }}" method="POST">
       {{ csrf_field() }}
           <div class="form-group">
@@ -25,8 +23,10 @@
           </div>
           <div class="form-group">
             <label name="message">Message:</label>
-            <textarea id="message" name="message" class="form-control">Type ur message</textarea>
+            <textarea id="message" name="message" class="form-control" placeholder="Let's talk!"></textarea>
           </div>
           <input type="submit" name="submit" value="Send" class="btn btn-success">
       </form>
+      </div>
+    </div> <!-- end of row -->
 @endsection
