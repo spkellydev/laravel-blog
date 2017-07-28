4a76c7926341ef3a03a55869997cef553599d49e
Sean Kelly <seanpkelly06@gmail.com>
2017-07-28 18:23:06 -0400

    metas and analytics

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -5,6 +5,18 @@
     <title>SPK.Design Blog | @yield('title')</title>
     <meta name="title" content="@yield('meta_title')">
     <meta name="description" content="@yield('meta_description')">
+    <meta name="google-site-verification" content="GLKQDRiETzjpek1H26BHJq-CqILEA_T8Map9zBMJFAQ" />
+    <meta name="msvalidate.01" content="5E13FF103B5744AF5D67F9A1FAC5ACBA" />
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-103580061-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     <!-- change this title for each page -->
 
     <!-- Bootstrap --> 
